8eb5ab2274f8eb0c95ee07782eadf7cc774ec778
touyou <fujiyou1121@gmail.com>
2019-10-08 18:54:23 +0900

    Initial Commit

---
+++ CocodaSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CocodaSwiftUI
+//
+//  Created by 藤井陽介 on 2019/10/08.
+//  Copyright © 2019 touyou. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
